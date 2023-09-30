a1b9715f91d2ddc693fcd2fb13aac9ef110cf6b3
youwkey <youwkey@gmail.com>
2023-09-30 23:56:03 +0900

    update: fzf git command

--- zsh/sync/function.zsh
+++ zsh/sync/function.zsh
@@ -3,10 +3,10 @@
 #==================================================================================================
 function fzf_ghq_open() {
   local -r FZF_PROMPT='Open Repository> '
-  local -r FZF_HEADER='<Ctrl-B>: Browse Repository'
+  local -r FZF_HEADER='<Ctrl-B>: Browse Repository, <Ctrl-p>: Browse Pull Requests'
   local -r PREVIEW='bat --color=always --number $(ghq root)/{}/README.md'
   local item=$(ghq list | fzf --prompt="${FZF_PROMPT}" --header "${FZF_HEADER}" \
     --preview "${PREVIEW}" \
-    --bind 'ctrl-b:execute(open https://{})+accept')
+    --bind 'ctrl-b:execute(open https://{})+accept,ctrl-p:execute(open https://{}/pull-requests)+accept')
   [[ ${item} ]] && cd $(ghq root)/${item} || :
 }
