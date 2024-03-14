52380dcc24b1c1a14c079a9ceea63d5be064556f
youwkey <youwkey@gmail.com>
2024-03-15 00:47:05 +0900

    feat: add fzf_ssh_connect()

--- zsh/sync/function.zsh
+++ zsh/sync/function.zsh
@@ -10,3 +10,10 @@ function fzf_ghq_open() {
     --bind 'ctrl-b:execute(open https://{})+accept,ctrl-p:execute(open https://{}/pull-requests)+accept')
   [[ ${item} ]] && cd $(ghq root)/${item} || :
 }
+
+function fzf_ssh_connect() {
+  local -r FZF_PROMPT='Connect To> '
+  local -r PREVIEW='echo ssh {}'
+  local item=$(cat ~/.ssh/config | grep '^Host' | cut -d ' ' -f2 | fzf --prompt="${FZF_PROMPT}" --preview "${PREVIEW}")
+  [[ ${item} ]] && ssh ${item} || :
+}
