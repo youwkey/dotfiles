0b937a026687403a3b2063fbc822973fd3b881be
youwkey <youwkey@gmail.com>
2026-02-22 20:55:36 +0900

    update: preview.sh for lf

--- lf/preview.sh
+++ lf/preview.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-bat --style=numbers,changes,header --color=always "$1"
+case "$(file --mime-type -b "$1")" in
+    image/*)          chafa --size "${2}x${3}" "$1" ;;
+    application/pdf)  pdftotext "$1" - ;;
+    application/zip)  unzip -l "$1" ;;
+    application/gzip) tar tzf "$1" ;;
+    *)                bat --style=numbers,changes,header --color=always "$1" ;;
+esac
