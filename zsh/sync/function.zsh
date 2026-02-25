765eb5ce859260224bf84effcc6c1a0229c45a89
youwkey <youwkey@gmail.com>
2026-02-25 13:30:46 +0900

    update: fzf_open function

--- zsh/sync/function.zsh
+++ zsh/sync/function.zsh
@@ -3,11 +3,14 @@
 #==================================================================================================
 function fzf_ghq_open() {
   local -r FZF_PROMPT='Open Repository> '
-  local -r FZF_HEADER='<Ctrl-B>: Browse Repository, <Ctrl-p>: Browse Pull Requests'
+  local -r FZF_HEADER='<Ctrl-B>: Browse Repository, <Ctrl-P>: Browse Pull Requests, <Ctrl-O>: Open in Zed'
   local -r PREVIEW='bat --color=always --number $(ghq root)/{}/README.md'
   local item=$(ghq list | fzf --prompt="${FZF_PROMPT}" --header "${FZF_HEADER}" \
     --preview "${PREVIEW}" \
-    --bind 'ctrl-b:execute(open https://{})+accept,ctrl-p:execute(open https://{}/pull-requests)+accept')
+    --bind 'ctrl-b:execute(open https://{})' \
+    --bind 'ctrl-p:execute(open https://{}/-/merge_requests)' \
+    --bind 'ctrl-o:execute(zed $(ghq root)/{})'
+    )
   [[ ${item} ]] && cd $(ghq root)/${item} || :
 }
 
