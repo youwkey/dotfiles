aa2754289db3392f64edc640da3f11a068ffa31e
youwkey <youwkey@gmail.com>
2026-03-16 21:46:27 +0900

    update: keybind for g command

--- zsh/sync/function.zsh
+++ zsh/sync/function.zsh
@@ -3,12 +3,12 @@
 #==================================================================================================
 function fzf_ghq_open() {
   local -r FZF_PROMPT='Open Repository> '
-  local -r FZF_HEADER='<Ctrl-B>: Browse Repository, <Ctrl-P>: Browse Pull Requests, <Ctrl-O>: Open in Zed'
+  local -r FZF_HEADER='<Ctrl-B>: Browse Repository, <Ctrl-M>: Browse Merge Requests, <Ctrl-O>: Open in Zed'
   local -r PREVIEW='bat --color=always --number $(ghq root)/{}/README.md'
   local item=$(ghq list | fzf --prompt="${FZF_PROMPT}" --header "${FZF_HEADER}" \
     --preview "${PREVIEW}" \
     --bind 'ctrl-b:execute(open https://{})' \
-    --bind 'ctrl-p:execute(open https://{}/-/merge_requests)' \
+    --bind 'ctrl-m:execute(open https://{}/-/merge_requests)' \
     --bind 'ctrl-o:execute(zed $(ghq root)/{})'
     )
   [[ ${item} ]] && cd $(ghq root)/${item} || :
